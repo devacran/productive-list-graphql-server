b39116fbd2687ecdd598b2aede52dbe967ffcf25
devacran <migueeldrg@gmail.com>
2020-11-10 11:49:17 -0600

    Preparing prod mode

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("graphql-tools");
 const { readFileSync } = require("fs");
 const { join } = require("path");
-const cors = require('cors')
 
-const app = express();
 const PORT = 6664;
+const isDev = process.env.NODE_ENV === "development";
+
+const cors = require("cors");
+const app = express();
 const typeDefs = readFileSync(join(__dirname, "schema.graphql"), "utf8");
 const resolvers = require("./resolvers");
 const schema = makeExecutableSchema({ typeDefs, resolvers });
-app.use(cors())
+
+app.use(cors());
 app.use(
   "/api",
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
   })
 );
 
